507fb73a4479a02f6e0b6ff9b5993c10c83c555e
rinashovik <rinashovik@gmail.com>
2023-09-26 19:42:31 -0500

    Final Commit "All tests passed"

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -5,10 +5,6 @@ import java.util.Objects;
 public class Location extends JobField{
 
 
-
-   // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
-         //constructor should also call the empty constructor in order to initialize the 'id' field.
-
         public Location(String value) {
 
             super(value);
